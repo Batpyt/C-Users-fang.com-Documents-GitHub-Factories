6bfe2ec0e1002c075322296b370b248d7eb16611
tianyuping <tianyuping@fang.com>
2020-09-28 10:54:33 +0800

    单例模式

---
+++ src/SingletonDemo.java
@@ -0,0 +1,22 @@
+class Singleton {
+    private static Singleton instance = new Singleton();// 在内部产生本类的实例化对象
+
+    public static Singleton getInstance() { // 通过静态方法返回instance对象
+        return instance;
+    }
+
+    private Singleton() { // 将构造方法封装为私有化
+    }
+
+    public void print() {
+        System.out.println("Hello World!!!");
+    }
+}
+
+
+public class SingletonDemo {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        s1.print();
+    }
+}
